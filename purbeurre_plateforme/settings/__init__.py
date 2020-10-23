d0d1912a7fa89d6da8a396c21857866bc15df47b
Dorel <dorel.paul@gmail.com>
2020-10-23 15:15:28 +0200

    dev key

--- purbeurre_plateforme/settings/__init__.py
+++ purbeurre_plateforme/settings/__init__.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '%vtal$5w@%5ldndcu^*$u@%xs^4rdgpaix^su0kdn7rb&rnjrs'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
